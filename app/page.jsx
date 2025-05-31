710db694f9960c9b4fc9346e3535b2ade647df55
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-05-31 23:46:14 +0600

    Enhance Hero Section with background image and improved text styling for better visual appeal

--- app/page.jsx
+++ app/page.jsx
@@ -16,15 +16,26 @@ export default async function HomePage() {
   return (
     <main className="p-6 text-white">
       {/* Hero Section */}
-      <section className="bg-gray-800 p-10 rounded-xl text-center">
-        <h1 className="text-4xl font-bold mb-4">Discover Premium Garments</h1>
-        <p className="text-lg mb-6">Stylish. Affordable. Comfortable.</p>
-        <Link
-          href="/category/men"
-          className="inline-block bg-black text-white px-6 py-3 rounded-full hover:black/70 transition"
-        >
-          Shop Now
-        </Link>
+      <section
+        className="relative bg-gray-800 p-10 rounded-xl text-center text-white bg-cover bg-center overflow-hidden "
+        style={{
+          backgroundImage:
+            "url('https://img.freepik.com/premium-photo/close-up-t-shirts-hangers-apparel-background_51195-3844.jpg?w=740')",
+        }}
+      >
+        <div className="bg-black/60 absolute h-full w-full top-0 left-0" />
+        <div className="relative z-10">
+          <h1 className="text-4xl font-bold mb-4">Discover Premium Garments</h1>
+          <p className="text-lg mb-6 font-medium italic">
+            Stylish. Affordable. Comfortable.
+          </p>
+          <Link
+            href="/category/men"
+            className="inline-block bg-indigo-500 hover:bg-indigo-600 text-white px-6 py-3 rounded-full hover:black/70 transition active:scale-95"
+          >
+            Shop Now
+          </Link>
+        </div>
       </section>
 
       {/* Featured Products */}
