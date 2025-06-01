7e02b9166b67a71bd10d089f8f53b90cc6c8f56b
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-06-01 23:23:54 +0600

    Add Providers component to wrap application with Redux store

---
+++ app/providers.js
@@ -0,0 +1,9 @@
+// app/providers.js
+"use client";
+
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
+
+export function Providers({ children }) {
+  return <Provider store={store}>{children}</Provider>;
+}
