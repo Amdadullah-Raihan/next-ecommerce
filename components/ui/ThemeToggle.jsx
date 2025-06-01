e4ea3e2f07cdd8c38622c3b108ad6775ca4e14f1
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-06-01 21:26:34 +0600

    Refactor ThemeToggle component for improved structure and styling

--- components/ui/ThemeToggle.jsx
+++ components/ui/ThemeToggle.jsx
@@ -1,8 +1,8 @@
-// components/ThemeToggle.js
 "use client";
 
 import { useEffect, useState } from "react";
-import { Moon, Sun } from "lucide-react";
+import { Sun, Moon } from "lucide-react";
+import { cn } from "@/utils/cn";
 
 export default function ThemeToggle() {
   const [dark, setDark] = useState(false);
@@ -16,6 +16,7 @@ export default function ThemeToggle() {
   const toggleTheme = () => {
     const newDark = !dark;
     setDark(newDark);
+
     localStorage.setItem("theme", newDark ? "dark" : "light");
     document.documentElement.classList.toggle("dark", newDark);
   };
@@ -23,10 +24,25 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 transition border border-gray-400 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700"
-      aria-label="Toggle Dark Mode"
+      aria-label="Toggle Theme"
+      className="relative flex items-center justify-center w-10 h-10 overflow-hidden "
     >
-      {dark ? <Sun size={18} /> : <Moon size={18} />}
+      {/* Sun icon */}
+      <Sun
+        size={24}
+        className={cn(
+          "absolute transition-all duration-300 transform",
+          dark ? "rotate-0 opacity-100" : "rotate-[45deg] opacity-0"
+        )}
+      />
+      {/* Moon icon */}
+      <Moon
+        size={24}
+        className={cn(
+          "absolute transition-all duration-300 transform",
+          dark ? "-rotate-[90deg] opacity-0" : "rotate-0 opacity-100"
+        )}
+      />
     </button>
   );
 }
