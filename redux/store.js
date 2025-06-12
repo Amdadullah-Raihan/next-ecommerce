19082280fe9fd6a1ededccfe13e20ae4eb275c85
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-06-12 20:54:36 +0600

    Add authApi integration to Redux store for enhanced authentication management

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./theme/themeSlice";
 import authReducer from "./auth/authSlice";
+import { authApi } from "./auth/authApiSlice";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
